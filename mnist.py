9400090dfd07b0716b880990bdcd0c9575c6497b
Bernardo Costa <bernardoantunescosta@gmail.com>
2019-05-27 17:44:30 -0300

    improving acc mnist

--- mnist.py
+++ mnist.py
@@ -5,10 +5,10 @@ from tensorflow.python.estimator.model_fn import ModeKeys as Modes
 INPUT_TENSOR_NAME = 'inputs'
 SIGNATURE_NAME = 'predictions'
 
+LEARNING_RATE = 0.001
+
+
 def model_fn(features, labels, mode, params):
-    # this script takes learning_rate as a hyperparameter
-    learning_rate = params.get("learning_rate",0.1)
-    
     # Input Layer
     input_layer = tf.reshape(features[INPUT_TENSOR_NAME], [-1, 28, 28, 1])
 
@@ -65,7 +65,7 @@ def model_fn(features, labels, mode, params):
             mode, predictions=predictions, export_outputs=export_outputs)
 
     if mode == Modes.TRAIN:
-        optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+        optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
         train_op = optimizer.minimize(loss, global_step=global_step)
         return tf.estimator.EstimatorSpec(mode, loss=loss, train_op=train_op)
 
@@ -119,3 +119,42 @@ def _input_fn(training_dir, training_filename, batch_size=100):
         capacity=1000 + 3 * batch_size)
 
     return {INPUT_TENSOR_NAME: images}, labels
+
+def neo_preprocess(payload, content_type):
+    import logging
+    import numpy as np
+    import PIL.Image   # Training container doesn't have this package
+    import io
+
+    logging.info('Invoking user-defined pre-processing function')
+
+    if content_type != 'application/x-image':
+        raise RuntimeError('Content type must be application/x-image')
+    
+    f = io.BytesIO(payload)
+    # Load image and convert to greyscale space
+    image = PIL.Image.open(f).convert('L')
+    # Resize
+    image = np.asarray(image.resize((28, 28)))
+    # Reshape
+    image = image.reshape((1,-1)).astype('float32')
+
+    return image
+
+### NOTE: this function cannot use MXNet
+def neo_postprocess(result):
+    import logging
+    import numpy as np
+    import json
+
+    logging.info('Invoking user-defined post-processing function')
+    
+    # Softmax (assumes batch size 1)
+    result = np.squeeze(result)
+    result_exp = np.exp(result - np.max(result))
+    result = result_exp / np.sum(result_exp)
+
+    response_body = json.dumps(result.tolist())
+    content_type = 'application/json'
+
+    return response_body, content_type
